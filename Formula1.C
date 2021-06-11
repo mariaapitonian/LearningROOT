a49c974d3a40e8c0d981abdda6f34120cea479c7
user <maria.apitonian2002@gmail.com>
2021-06-11 20:14:15 +0400

    TF1 Formula Class (Lesson 1)

--- Formula1.C
+++ Formula1.C
@@ -1,6 +1,66 @@
-//Example: Use of ROOT TFormula class to declare and plot a 1-Dim TF1 function
+//Example: Use of ROOT TFormula class to declare and plot 1-Dim Surface TF1 functions
 
 {
-	f1 = new TF1("f1", "sin(x)", -6, 6);
+	/*f1 = new TF1("f1", "3*sin(x)", -6, 6);
+	f1->SetLineColor(kBlue+2);
+	f1->SetLineWidth(3);
+	f1->SetTitle("Displacement of objects 1 and 2; Time, t (s); Displacement, x (m)");
 	f1->Draw();
+
+
+	f2 = new TF1("f2", "2*cos(x)", -6, 6);
+	f2->SetLineColor(kRed);
+	f2->SetLineWidth(3);
+	f2->SetTitle("Displacement of objects 1 and 2; Time, t (s); Displacement, x (m)");
+	f2->Draw("SAME");  //Allows ROOT to draw both of these functions on the same canva
+
+*/
+
+
+
+
+	/*In order to draw on the same canva but side by side, the following code would have to be added
+	c1 = new TCanvas("c1", "Displacement of objects", 1000, 500);
+	c1->Divide(3,1);  //what this does is it divides the canva into two columns and one row, plotting the functions in the order with which they are specified
+
+	c1->cd(1);
+	//define f1
+
+	c1->cd(2);
+	//define f2
+	f2->Draw(); //without "SAME"
+	
+	//Note: In this case we can also set separate titles for the graphs instead of the title being determined by whatever is written in f1*/
+
+
+
+
+
+
+
+	//In order to draw on the same canva but side by side, the following code would have to be added
+        c1 = new TCanvas("Formula1_TwoFunctionsSeparate", "Displacement of objects", 1100, 500);
+        c1->Divide(2,1);  //what this does is it divides the canva into two columns and one row, plotting the functions in the order with which they are specified
+
+        c1->cd(1);
+        //define f1
+	f1 = new TF1("f1", "3*sin(x)", -6, 6);
+        f1->SetLineColor(kBlue+2);
+        f1->SetLineWidth(3);
+        f1->SetTitle("Displacement of object 1; Time, t (s); Displacement, x (m)");
+        f1->Draw();
+
+
+        c1->cd(2);
+        //define f2
+	f2 = new TF1("f2", "2*cos(x)", -6, 6);
+        f2->SetLineColor(kRed);
+        f2->SetLineWidth(3);
+        f2->SetTitle("Displacement of object 2; Time, t (s); Displacement, x (m)");
+        f2->Draw(); //without "SAME"
+        
+        //Note: In this case we can also set separate titles for the graphs instead of the title being determined by whatever is written in f1*/
+
+
+
 }
--- Formula1.md
+++ Formula1.md
@@ -1,9 +1,14 @@
 TF1
 - Formula class in ROOT
-- Plots 1-Dim function defined between lower and upper limit
+- Plots function on a 1-Dim defined between lower and upper limit
+- Lower and upper limits for x are given, range for y is automatically determined but can be edited
 
 Macro file can be edited to edit the plotted function
 OR canvas can be opened and edited through view-> editor (this is easier if you don't know color codes etc)
 --> the macro of the edited version can also be saved in order to have that design
 
 In the original document, we can also put something like f1->SetLineColor(kBlue+3) before f->Draw() in order to edit specific features from the default.
+
+
+For colors, see wheel.png or go to View->Colors on any canva
+
